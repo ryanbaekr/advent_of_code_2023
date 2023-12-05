55a12d5f475d564412c039a0d6ace7c1071fa3dc
Ryan Baker <ryanbaekr@gmail.com>
2023-12-04 19:24:29 -0500

    day_04 optimization

--- aoc/day_04.py
+++ aoc/day_04.py
@@ -14,12 +14,9 @@ def scratchcards(tickets: str) -> tuple[int, int]:
 
     for l_num, line in enumerate(lines):
         line = line.split(": ")[-1].split(" | ")
-        my_nums = [int(num) for num in line[0].split(" ")]
-        winning_nums = [int(num) for num in line[1].split(" ")]
-        temp = len(set(my_nums) & set(winning_nums))
-        if temp:
-            points += 2**(temp - 1)
-        for jdx in range(temp):
-            scrots[l_num+jdx+1] += 1 * scrots[l_num]
+        matches = len(set(line[0].split(" ")) & set(line[1].split(" ")))
+        points += int(2**(matches - 1))
+        for jdx in range(l_num + 1, matches + l_num + 1):
+            scrots[jdx] += scrots[l_num]
 
     return points, sum(scrots)
