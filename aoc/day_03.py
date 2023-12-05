27d91cca50fe5b084089c8ac8e295eceec82065f
Ryan Baker <ryanbaekr@gmail.com>
2023-12-04 19:24:39 -0500

    day_03 optimization

--- aoc/day_03.py
+++ aoc/day_03.py
@@ -10,37 +10,37 @@ def gear_ratios(schematic: str) -> tuple[int, int]:
     part_sum = 0
     gear_sum = 0
 
-    for l_num in range(len(lines) - 2):
-        last_line = lines[l_num]
-        this_line = lines[l_num + 1]
-        next_line = lines[l_num + 2]
-        for c_num in range(len(this_line) - 6):
-            if this_line[c_num + 3] not in ["*", "/", "$", "+", "&", "@", "#", "%", "=", "-"]:
+    for l_num in range(1, len(lines) - 1):
+        last_line = lines[l_num - 1]
+        this_line = lines[l_num]
+        next_line = lines[l_num + 1]
+        for c_num in range(3, len(this_line) - 3):
+            if this_line[c_num] not in ["*", "/", "$", "+", "&", "@", "#", "%", "=", "-"]:
                 continue
 
             gear_pwr = [0]
-            if this_line[c_num + 2].isdigit():
-                attempt = int(re.sub(r"^\d?[^\d]+", "", this_line[c_num:c_num + 3]))
+            if this_line[c_num - 1].isdigit():
+                attempt = int(re.sub(r"^\d?[^\d]+", "", this_line[c_num - 3:c_num]))
                 part_sum += attempt
                 gear_pwr.append(attempt)
-            if this_line[c_num + 4].isdigit():
-                attempt = int(re.sub(r"[^\d]+\d?$", "", this_line[c_num + 4:c_num + 7]))
+            if this_line[c_num + 1].isdigit():
+                attempt = int(re.sub(r"[^\d]+\d?$", "", this_line[c_num + 1:c_num + 4]))
                 part_sum += attempt
                 gear_pwr.append(attempt)
             for line in (last_line, next_line):
-                if line[c_num + 3].isdigit():
-                    attempt = int(re.sub(r"(^\d?[^\d]+|[^\d]+\d?$)", "", line[c_num + 1:c_num + 6]))
+                if line[c_num].isdigit():
+                    attempt = int(re.sub(r"(^\d?[^\d]+|[^\d]+\d?$)", "", line[c_num - 2:c_num + 3]))
                     part_sum += attempt
                     gear_pwr.append(attempt)
                 else:
-                    if line[c_num + 2].isdigit():
-                        attempt = int(re.sub(r"^\d?[^\d]+", "", line[c_num:c_num + 3]))
+                    if line[c_num - 1].isdigit():
+                        attempt = int(re.sub(r"^\d?[^\d]+", "", line[c_num - 3:c_num]))
                         part_sum += attempt
                         gear_pwr.append(attempt)
-                    if line[c_num + 4].isdigit():
-                        attempt = int(re.sub(r"[^\d]+\d?$", "", line[c_num + 4:c_num + 7]))
+                    if line[c_num + 1].isdigit():
+                        attempt = int(re.sub(r"[^\d]+\d?$", "", line[c_num + 1:c_num + 4]))
                         part_sum += attempt
                         gear_pwr.append(attempt)
-            gear_sum += gear_pwr[-2] * gear_pwr[-1] * (this_line[c_num + 3] == "*")
+            gear_sum += gear_pwr[-2] * gear_pwr[-1] * (this_line[c_num] == "*")
 
     return part_sum, gear_sum
