44c06052148e3b8b89be3e4849dac158c8ea1ab2
Ryan Baker <ryanbaekr@gmail.com>
2023-12-10 21:04:50 -0500

    support extra arguments for timing test

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--fun", type=str, required=True)
 parser.add_argument("--inp", type=str, required=True)
 parser.add_argument("--num", type=int, default=1000)
-args, _ = parser.parse_known_args()
+args, optional_args = parser.parse_known_args()
 
 with open(args.inp, encoding="utf-8") as f:
     inp = f.read()
 
-print(timeit.timeit(lambda: getattr(aoc, args.fun)(inp), number=args.num))
+print(timeit.timeit(lambda: getattr(aoc, args.fun)(inp, *optional_args), number=args.num))
