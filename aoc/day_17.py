8507602ed3be95b25b6048eed206c705c321ad84
Ryan Baker <ryanbaekr@gmail.com>
2024-07-02 13:38:10 -0400

    day_17 optimization

--- aoc/day_17.py
+++ aoc/day_17.py
@@ -5,88 +5,99 @@ import heapq
 def clumsy_crucible(city_map: str, ultra: bool) -> int:
     """Take the map and return the appropriate value"""
 
+    if ultra:
+        min_dur = 4
+        max_dur = 10
+    else:
+        min_dur = 1
+        max_dur = 3
+
     city_map = [[int(char) for char in line] for line in city_map.splitlines()]
 
-    loss_list = [(0, " 9", 0, 0)]
+    loss_list = [(0, " ", 9, 0, 0)]
 
     seen = set()
-    mins = {(" 9", 0, 0): 0}
+    mins = {(" ", 9, 0, 0): 0}
 
     while True:
-        loss, path, l_idx, c_idx = heapq.heappop(loss_list)
+        loss, rot, dur, l_idx, c_idx = heapq.heappop(loss_list)
 
-        if (path, l_idx, c_idx) in seen:
+        if (rot, dur, l_idx, c_idx) in seen:
             continue
 
         if l_idx == 140 and c_idx == 140:
             return loss
 
         # up
-        if l_idx > 0 and path[0] != "d" and ((not ultra and path != "u3") or (ultra and path != "u10" and (int(path[1:]) >= 4 or path[0] == "u"))):
-            if path[0] == "u":
-                new_path = f"u{int(path[1:]) + 1}"
+        if l_idx > 0 and rot != "d" and ((rot != "u" and dur >= min_dur) or (rot == "u" and dur < max_dur)):
+            new_rot = "u"
+            if rot == "u":
+                new_dur = dur + 1
             else:
-                new_path = "u1"
+                new_dur = 1
             new_l_idx = l_idx - 1
             new_c_idx = c_idx
-            new_name = (new_path, new_l_idx, new_c_idx)
+            new_name = (new_rot, new_dur, new_l_idx, new_c_idx)
 
             if new_name not in seen:
                 prev = mins.get(new_name)
                 new_loss = loss + city_map[new_l_idx][new_c_idx]
                 if prev is None or new_loss < prev:
                     mins[new_name] = new_loss
-                    heapq.heappush(loss_list, (new_loss, new_path, new_l_idx, new_c_idx))
+                    heapq.heappush(loss_list, (new_loss, new_rot, new_dur, new_l_idx, new_c_idx))
 
         # down
-        if l_idx < 140 and path[0] != "u" and ((not ultra and path != "d3") or (ultra and path != "d10" and (int(path[1:]) >= 4 or path[0] == "d"))):
-            if path[0] == "d":
-                new_path = f"d{int(path[1:]) + 1}"
+        if l_idx < 140 and rot != "u" and ((rot != "d" and dur >= min_dur) or (rot == "d" and dur < max_dur)):
+            new_rot = "d"
+            if rot == "d":
+                new_dur = dur + 1
             else:
-                new_path = "d1"
+                new_dur = 1
             new_l_idx = l_idx + 1
             new_c_idx = c_idx
-            new_name = (new_path, new_l_idx, new_c_idx)
+            new_name = (new_rot, new_dur, new_l_idx, new_c_idx)
 
             if new_name not in seen:
                 prev = mins.get(new_name)
                 new_loss = loss + city_map[new_l_idx][new_c_idx]
                 if prev is None or new_loss < prev:
                     mins[new_name] = new_loss
-                    heapq.heappush(loss_list, (new_loss, new_path, new_l_idx, new_c_idx))
+                    heapq.heappush(loss_list, (new_loss, new_rot, new_dur, new_l_idx, new_c_idx))
 
         # left
-        if c_idx > 0 and path[0] != "r" and ((not ultra and path != "l3") or (ultra and path != "l10" and (int(path[1:]) >= 4 or path[0] == "l"))):
-            if path[0] == "l":
-                new_path = f"l{int(path[1:]) + 1}"
+        if c_idx > 0 and rot != "r" and ((rot != "l" and dur >= min_dur) or (rot == "l" and dur < max_dur)):
+            new_rot = "l"
+            if rot == "l":
+                new_dur = dur + 1
             else:
-                new_path = "l1"
+                new_dur = 1
             new_l_idx = l_idx
             new_c_idx = c_idx - 1
-            new_name = (new_path, new_l_idx, new_c_idx)
+            new_name = (new_rot, new_dur, new_l_idx, new_c_idx)
 
             if new_name not in seen:
                 prev = mins.get(new_name)
                 new_loss = loss + city_map[new_l_idx][new_c_idx]
                 if prev is None or new_loss < prev:
                     mins[new_name] = new_loss
-                    heapq.heappush(loss_list, (new_loss, new_path, new_l_idx, new_c_idx))
+                    heapq.heappush(loss_list, (new_loss, new_rot, new_dur, new_l_idx, new_c_idx))
 
         # right
-        if c_idx < 140 and path[0] != "l" and ((not ultra and path != "r3") or (ultra and path != "r10" and (int(path[1:]) >= 4 or path[0] == "r"))):
-            if path[0] == "r":
-                new_path = f"r{int(path[1:]) + 1}"
+        if c_idx < 140 and rot != "l" and ((rot != "r" and dur >= min_dur) or (rot == "r" and dur < max_dur)):
+            new_rot = "r"
+            if rot == "r":
+                new_dur = dur + 1
             else:
-                new_path = "r1"
+                new_dur = 1
             new_l_idx = l_idx
             new_c_idx = c_idx + 1
-            new_name = (new_path, new_l_idx, new_c_idx)
+            new_name = (new_rot, new_dur, new_l_idx, new_c_idx)
 
             if new_name not in seen:
                 prev = mins.get(new_name)
                 new_loss = loss + city_map[new_l_idx][new_c_idx]
                 if prev is None or new_loss < prev:
                     mins[new_name] = new_loss
-                    heapq.heappush(loss_list, (new_loss, new_path, new_l_idx, new_c_idx))
+                    heapq.heappush(loss_list, (new_loss, new_rot, new_dur, new_l_idx, new_c_idx))
 
-        seen.add((path, l_idx, c_idx))
+        seen.add((rot, dur, l_idx, c_idx))
